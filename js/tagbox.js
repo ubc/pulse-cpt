6caf14edbb92ad072ad9d31264a0ae5a317817d6
Buğra Fırat <ebemunk@gmail.com>
2012-12-15 02:32:26 -0800

    tagbox revert

--- js/tagbox.js
+++ js/tagbox.js
@@ -1,19 +1,19 @@
 /*
-*   Based on the work by Daniel Stocks
+* Based on the work by Daniel Stocks
+* jQuery tagbox
+* -------------
+* Released under the MIT, BSD, and GPL Licenses.
+* Copyright 2011 Daniel Stocks
 *
-*   jQuery tagbox
-*   -------------
-*   Released under the MIT, BSD, and GPL Licenses.
-*   Copyright 2011 Daniel Stocks
-*   http://webcloud.se/code/jQuery-TaggingTools/
+* Dependencies:
+* ------------
+* jquery.autoGrowInput.js
 *
-*   
-*  
 */
 
 (function($) {
     
-    function TagBox( input, options) {
+    function TagBox(input, options) {
 
         var self = this;
         
@@ -22,39 +22,28 @@
         self.delimit_expr = /\s*,\s*/
 
         if(options.delimit_by_space) {
-            self.delimit_key = 32 
+            self.delimit_key = 32
             self.delimit_expr = /\s+/
         }
 
         var val = input.val();
         var tags = []
-        if(val) { 
-            tags = input.val().split( self.delimit_expr );
+        if(val) {
+            tags = input.val().split(self.delimit_expr);
         }
         self.input = input
         self.tagInput = $('<input>', {
             'type' : 'text',
-            'title' : 'separate '+options.single_input+' by comma',
-            'name': 'single_'+options.single_input,
             'keydown' : function(e) {
-                if(e.keyCode == self.delimit_key ) {
+                if(e.keyCode == 13 || e.keyCode == self.delimit_key ) {
                     $(this).trigger("selectTag");
-                    
                     e.preventDefault();
                 }
-                if(self.tagInput.val() == '' && e.keyCode == 8){
-                	var last_tag_index =  self.tagbox.find("li").length - 2; // -2 because we ignore the input 
-                	if(last_tag_index >= 0){
-                		self.removeTag(last_tag_index);
-                	}
-                	
-                	e.preventDefault();
-                }
+            },
+            'blur' : function(e) {
+                $(this).val("");
             }
         });
-        input.parents('form:first').bind("submit", function() { self.removeAllTags(); })
-        //console.log(  );
-
         
         self.tagInput.bind("selectTag", function() {
             if(!$(this).val()) {
@@ -74,11 +63,10 @@
         self.tags = []
         
         input.after(self.tagbox).hide();
-        
+
         self.inputHolder = $('<li class="input">');
         self.tagbox.append(self.inputHolder);
         self.inputHolder.append(self.tagInput);
-        
         self.tagInput.autoGrowInput();
         
         for(tag in tags) {
@@ -109,7 +97,7 @@
             self.updateInput();
         },
         removeTag : function(index) {
-           
+            
             this.tagbox.find("li").eq(index).remove();
             this.tags.splice(index, 1);
             this.updateInput();
@@ -122,25 +110,16 @@
             } else {
                 tags = this.tags.join(",");
             }
-            
-            this.input.text(tags);
-            this.options.update( tags );
-        },
-        removeAllTags: function () {
-        	var i = 0;
-        	this.tags = []; // empty array
-        	this.tagbox.find("li.tag").remove();
-        	this.updateInput();
+            this.input.val(tags);
         }
     }
     
-    $.fn.tagbox = function(options) {
+    $.fn.tagBox = function(options) {
+
         var defaults = {
-            delimit_by_space : false,
-            update : function() {},
-            single_input : ''
+            delimit_by_space : false
         }
-        var options = $.extend(defaults, options );
+        var options = $.extend(defaults, options);
         return this.each(function() {
             
             var input = $(this);
